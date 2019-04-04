ccc5ac40f34451e1fa0f8ff389da5757345747c6
James Herdman <james.herdman@gmail.com>
2019-04-04 17:57:30 +0000

    Account for HEIC Images
    No browser vendors support rendering these images, so we need to convert
    them into something meaningful. I'm arbitrarily picking JPEG format.
    
    SP-837

--- tests/integration/components/imgix-photo-test.js
+++ tests/integration/components/imgix-photo-test.js
@@ -79,4 +79,71 @@ module('Integration | Component | imgix-photo', function(hooks) {
 
     assert.dom('img').hasAttribute('src', expectedSrc);
   });
+
+  test('converts HEIC images auto-magically', async function(assert) {
+    let heicUrl = 'http://example.com/image.heic';
+
+    this.setProperties({
+      alt: 'Test!',
+      url: heicUrl,
+      w: 300,
+      h: 400,
+      fit: 'crop',
+      sepia: 88,
+    });
+
+
+    let dpr = window.devicePixelRatio;
+
+    let expectedSrc = `${heicUrl}?dpr=${dpr}&fit=crop&fm=jpg&h=400&sepia=88&w=300`;
+
+    await render(hbs`<ImgixPhoto
+      @autoSetDimensions={{false}}
+      @url={{url}}
+      @alt={{alt}}
+      @params={{hash
+        w=w
+        h=h
+        fit=fit
+        sepia=sepia
+      }}
+      class="SomeClass"
+    />`);
+
+    assert.dom('img').hasAttribute('src', expectedSrc);
+  });
+
+  test('does not stomp on `fm` if provided for a HEIC image', async function(assert) {
+    let heicUrl = 'http://example.com/image.heic';
+
+    this.setProperties({
+      alt: 'Test!',
+      url: heicUrl,
+      w: 300,
+      h: 400,
+      fit: 'crop',
+      fm: 'png',
+      sepia: 88,
+    });
+
+    let dpr = window.devicePixelRatio;
+
+    let expectedSrc = `${heicUrl}?dpr=${dpr}&fit=crop&fm=png&h=400&sepia=88&w=300`;
+
+    await render(hbs`<ImgixPhoto
+      @autoSetDimensions={{false}}
+      @url={{url}}
+      @alt={{alt}}
+      @params={{hash
+        w=w
+        h=h
+        fit=fit
+        fm=fm
+        sepia=sepia
+      }}
+      class="SomeClass"
+    />`);
+
+    assert.dom('img').hasAttribute('src', expectedSrc);
+  });
 });
